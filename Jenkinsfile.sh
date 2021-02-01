771cbe7748e2174f8de13cc8ac1cc4f6d6ad3864
siva9427 <sivasukumarreddy.poreddy@objectrocket.com>
2021-02-01 14:50:09 +0530

    OR5-246 commit

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -4,11 +4,17 @@ export IMAGE_TAG="sensu-python-runtime:${PYTHON_VERSION}-${OSVERSION}"
 export CONTAINER_NAME="sensu-python-runtime_${PYTHON_VERSION}-${OSVERSION}"
 export S3_FILE_URL=https://${S3_BUCKET}.s3.amazonaws.com${S3_FOLDER}${ASSET_FILE_NAME}
 export PATH=/var/lib/jenkins/.local/bin:$PATH
+
+# Installing awscli to push sensu-runtime asset to S3 bucket
 pip install --user awscli
+
+# Docker build and push commands
 docker build --build-arg "ASSET_VERSION=${ASSET_VERSION}" --build-arg "PYTHON_VERSION=${PYTHON_VERSION}" -t ${IMAGE_TAG} -f Dockerfile.${OSVERSION} .
 docker run -itd --name ${CONTAINER_NAME} ${IMAGE_TAG}
 docker cp ${CONTAINER_NAME}:/assets/${ASSET_FILE_NAME} .
 docker rm -f ${CONTAINER_NAME}
+
+# S3 upload
 echo "Uploading sensu asset to S3 bucket: ${S3_BUCKET}"
 aws s3 cp ${ASSET_FILE_NAME} ${S3_LOCATION} --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 rm -rf ${ASSET_FILE_NAME}
